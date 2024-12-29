a4d86adac6ac97f793f03f7e2e7bc4ac526e697a
jefolidev <jefoliveira279@gmail.com>
2024-12-29 19:01:01 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa3

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'presente-namorado',
+  base: '/presente-namorado',
   build: {
     outDir: 'dist',
   },
